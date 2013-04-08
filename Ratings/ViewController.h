6b54cf6ac81022e63ad3dd7221b42479158c5cfc
IOS <IOS@bb215l28.student.slcc.int>
2013-04-08 17:40:41 -0600

    Initial Commit

---
+++ Ratings/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Ratings
+//
+//  Created by IOS on 4/8/13.
+//  Copyright (c) 2013 IOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
